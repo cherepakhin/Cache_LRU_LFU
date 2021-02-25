a31f7a8c6417599f8aff8600fbb490bdd4c977a2
Черепахин Василий Геннадьевич <vasilii.cherepakhin@domru.ru>
2021-02-25 17:13:57 +0500

    Заготовка

---
+++ src/test/java/ru/AppTest.java
@@ -0,0 +1,12 @@
+package ru;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
